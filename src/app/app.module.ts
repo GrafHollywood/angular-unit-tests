dfd49597479e72a670fbfa9bdd0ed8534a30d7d1
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2023-03-11 14:29:42 +0300

    remove posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
